22e8f09691f03217c1ee2c8d1e1b35185726e1ea
Piskka <39105888+wxin9monash@users.noreply.github.com>
2023-05-23 02:48:44 +1000

    change font color

--- screens/home/homeScreen.js
+++ screens/home/homeScreen.js
@@ -627,7 +627,7 @@ const styles = StyleSheet.create({
     },
     tabText: {
         fontSize: 14,
-        color: 'grey',
+        color: 'lightgrey',
         fontFamily: 'OpenSans_SemiBold',
     },
     activeTabText: {
